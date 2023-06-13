0649ca0ac58cb093322263b71fb3f10006e221fc
lucas-morais <lucasmoraisss@gmail.com>
2023-06-13 16:30:33 -0300

    start: project basic configuration - backend(Spring) | frontend(NextJs)

---
+++ backend/src/main/java/com/github/lucasmoraisss/smartflix/SmartflixApplication.java
@@ -0,0 +1,13 @@
+package com.github.lucasmoraisss.smartflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartflixApplication.class, args);
+	}
+
+}
